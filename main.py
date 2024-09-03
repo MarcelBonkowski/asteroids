0e867155c8e42ad5bff15abbe7a305f5ea26f192
Marcel Bonkowski <marcel.bonkowski@web.de>
2024-09-03 18:30:44 +0200

    add collision

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
+import sys
 from constants import *
 from player import Player
 
@@ -10,10 +13,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     dt = 0
 
@@ -24,6 +31,10 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for obj in asteroids:
+            if obj.is_colliding(player):
+                sys.exit("Game over!")
 
         screen.fill("black")
 
